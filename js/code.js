9fcba946458b88ce8b0601ea67286ba155f50a5b
root <stevens33458@gmail.com>
2023-02-09 09:25:24 +0000

    FUCK

--- js/code.js
+++ js/code.js
@@ -4,6 +4,84 @@ const extension = 'php';
 let userId = 0;
 let firstName = "";
 let lastName = "";
+function searchContact()
+{
+	let srch = document.getElementById("searchText").value;
+	document.getElementById("colorSearchResult").innerHTML = "";
+	
+	let colorList = "";
+
+	let tmp = {search:srch,userId:userId};
+	let jsonPayload = JSON.stringify( tmp );
+
+	let url = urlBase + 'LAMPAPI/SearchContact.' + extension;
+	
+	let xhr = new XMLHttpRequest();
+	xhr.open("POST", url, true);
+	xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+	try
+	{
+		xhr.onreadystatechange = function() 
+		{
+			if (this.readyState == 4 && this.status == 200) 
+			{
+				document.getElementById("colorSearchResult").innerHTML = "Color(s) has been retrieved";
+				let jsonObject = JSON.parse( xhr.responseText );
+				
+				for( let i=0; i<jsonObject.results.length; i++ )
+				{
+					colorList += jsonObject.results[i];
+					if( i < jsonObject.results.length - 1 )
+					{
+						colorList += "<br />\r\n";
+					}
+				}
+				
+				document.getElementsByTagName("p")[0].innerHTML = colorList;
+			}
+		};
+		xhr.send(jsonPayload);
+	}
+	catch(err)
+	{
+		document.getElementById("colorSearchResult").innerHTML = err.message;
+	}
+}
+function addContact()
+{
+	/*contactName" placeholder="Contact Name">
+    	<input type="text" id="contactPhone" placeholder="Contact Phone">
+    	<input type="text" id="contactEmail*/
+	let newName = document.getElementById("contactName").value;
+	let newPhone = document.getElementById("contactPhone").value;
+	let newEmail = document.getElementById("contactEmail").value;
+	document.getElementById("colorAddResult").innerHTML = "";
+
+	//let tmp = {color:newColor,userId,userId};
+	let tmp = {contactName:newName,contactPhone:newPhone,contactEmail:newEmail,userId:userId}
+	let jsonPayload = JSON.stringify( tmp );
+
+	let url = urlBase + 'LAMPAPI/AddContact.' + extension;
+
+	let xhr = new XMLHttpRequest();
+	xhr.open("POST", url, true);
+	xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+	try
+	{
+		xhr.onreadystatechange = function()
+		{
+			if (this.readyState == 4 && this.status == 200)
+			{
+				document.getElementById("colorAddResult").innerHTML = "Color has been added";
+			}
+		};
+		xhr.send(jsonPayload);
+	}
+	catch(err)
+	{
+		document.getElementById("colorAddResult").innerHTML = err.message;
+	}
+}
 function doSignUp(){
 	userId = 0;
 	firstName = "";
@@ -98,7 +176,7 @@ function doLogin()
 
 				saveCookie();
 	
-				window.location.href = "color.html";
+				window.location.href = "contact.html";
 			}
 		};
 		xhr.send(jsonPayload);
