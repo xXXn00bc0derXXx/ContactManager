aa177969a975c445c9f19d0a4415a80ceef62765
root <stevens33458@gmail.com>
2023-02-10 17:14:21 +0000

    Almost there...

--- LAMPAPI/SearchContact.php
+++ LAMPAPI/SearchContact.php
@@ -4,7 +4,7 @@
 	
 	$searchResults = "";
 	$searchCount = 0;
-
+	$searchID = "";
 	$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331");
 	if ($conn->connect_error) 
 	{
@@ -12,7 +12,7 @@
 	} 
 	else
 	{
-		$stmt = $conn->prepare("select Name,Phone,Email from Contacts where Name like ? and UserID=?");
+		$stmt = $conn->prepare("select ID,Name,Phone,Email from Contacts where Name like ? and UserID=?");
 		$contactName = "%" . $inData["search"] . "%";
 		$stmt->bind_param("ss", $contactName, $inData["userId"]);
 		$stmt->execute();
@@ -24,9 +24,11 @@
 			if( $searchCount > 0 )
 			{
 				$searchResults .= ",";
+				$searchID .= ",";
 			}
 			$searchCount++;
 			$searchResults .= '"' . $row["Name"] .' '. $row["Phone"] .' '. $row["Email"]. '"';
+			$searchID .= '"' . $row["ID"] . '"';
 		}
 		
 		if( $searchCount == 0 )
@@ -35,7 +37,7 @@
 		}
 		else
 		{
-			returnWithInfo( $searchResults );
+			returnWithInfo( $searchResults, $searchID );
 		}
 		
 		$stmt->close();
@@ -59,9 +61,9 @@
 		sendResultInfoAsJson( $retValue );
 	}
 	
-	function returnWithInfo( $searchResults )
+	function returnWithInfo( $searchResults, $searchID )
 	{
-		$retValue = '{"results":[' . $searchResults . '],"error":""}';
+		$retValue = '{"results":[' . $searchResults . '],"id":[' . $searchID . '],"error":""}';
 		sendResultInfoAsJson( $retValue );
 	}
 	
