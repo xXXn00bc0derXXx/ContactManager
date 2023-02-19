7a515aac6fe6e463679afac3a85bbdebd642ced9
root <stevens33458@gmail.com>
2023-02-19 02:40:10 +0000

    Edit Working kinda

--- LAMPAPI/UpdateContact.php
+++ LAMPAPI/UpdateContact.php
@@ -1,10 +1,42 @@
-<html>
-<body>
-<form name ="form" action="" method ="post">
-	<input type="text" id="contactName" placeholder="Contact Name">
-    	<input type="text" id="contactPhone" placeholder="Contact Phone">
-	<input type="text" id="contactEmail" placeholder="E-mail">
-	<input type="submit" name ="Submit Change" value="click"/>
-</form>
-</body>
-</html>
+<?php
+	$inData = getRequestInfo();
+	
+	$contactName = $inData["editName"];
+    	$contactPhone =$inData["editPhone"];
+    	$contactEmail = $inData["editEmail"];
+	$userId = $inData["userId"];
+    	$changeId = $inData["cUserID"];
+
+	$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331");
+	if ($conn->connect_error) 
+	{
+		returnWithError( $conn->connect_error );
+	} 
+	else
+	{
+		$stmt = $conn->prepare("UPDATE Contacts SET Name = ?, Phone = ?, Email=? WHERE userID = ? AND ID = ?");
+		$stmt->bind_param("sssss", $contactName, $contactPhone,$contactEmail,$userId,$changeId);
+		$stmt->execute();
+		$stmt->close();
+		$conn->close();
+		returnWithError("");
+	}
+
+	function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
+
+	function sendResultInfoAsJson( $obj )
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
+	
+	function returnWithError( $err )
+	{
+		$retValue = '{"error":"' . $err . '"}';
+		sendResultInfoAsJson( $retValue );
+	}
+	
+?>
